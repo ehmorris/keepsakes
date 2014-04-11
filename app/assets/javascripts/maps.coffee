f467e46d0fca1e929dd6ef64f0b9cdda1bd7b08a
Edwin <edwin@rapgenius.com>
2014-04-10 22:04:00 -0400

    differentiate walking from transport

--- app/assets/javascripts/maps.coffee
+++ app/assets/javascripts/maps.coffee
@@ -15,17 +15,25 @@ $ ->
 
   # aggregate all geodata into one object to determine boundaries
   $.each geodata_json, ->
+    if @.activity == 'wlk'
+      properties = {
+        'title': @.title
+        'stroke': '#fff'
+        'stroke-opacity': .9
+        'stroke-width': 5}
+    else
+      properties = {
+        'title': @.title
+        'stroke': '#999'
+        'stroke-opacity': .5
+        'stroke-width': 5}
+
     window.feature_layer = L.mapbox.featureLayer {
       type: 'FeatureCollection',
       features: window.feature_layer.getGeoJSON().features.concat {
         type: 'Feature',
         geometry: @,
-        properties: {
-          'title': @.title
-          'stroke': '#fff'
-          'stroke-opacity': .9
-          'stroke-width': 5
-        }}}
+        properties: properties}}
 
   # zoom the map to fit the boundaries, but don't plot any points
   window.map.fitBounds(window.feature_layer.getBounds())
--- lib/maps.rb
+++ lib/maps.rb
@@ -3,21 +3,24 @@ module Maps
     lon = segment['place']['location']['lon']
     lat = segment['place']['location']['lat']
     title = segment['place']['name']
+    type = 
 
     {'type' => 'Point',
      'coordinates' => [lon, lat],
      'title' => title}
   end
 
-  def trackpoints_to_geodata_line(trackpoint_1, trackpoint_2)
+  def trackpoints_to_geodata_line(trackpoint_1, trackpoint_2, activity)
     beginning_lon = trackpoint_1['lon']
     beginning_lat = trackpoint_1['lat']
     end_lon = trackpoint_2['lon']
     end_lat = trackpoint_2['lat']
 
-    {'type' => 'LineString', 'coordinates' => [
+    {'type' => 'LineString',
+     'coordinates' => [
       [beginning_lon, beginning_lat],
-      [end_lon, end_lat]]}
+      [end_lon, end_lat]],
+     'activity' => activity}
   end
 
   def storyline_to_geodata(storyline_segments_hash)
@@ -27,12 +30,14 @@ module Maps
         geodata_hash.push(place_to_geodata_point(segment))
       elsif segment['type'] == 'move'
         segment['activities'].each do |activity|
+          activity_type = activity['activity']
           activity['trackPoints'].each_with_index do |trackpoint, i|
             unless activity['trackPoints'][i + 1].nil?
               geodata_hash.push(
                 trackpoints_to_geodata_line(
                   trackpoint,
-                  activity['trackPoints'][i + 1]))
+                  activity['trackPoints'][i + 1],
+                  activity_type))
             end
           end
         end
