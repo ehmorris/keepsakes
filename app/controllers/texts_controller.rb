ae556dbc2af5e5bb8a37a418f9242de73057ffa8
Edwin <edwin.h.morris@gmail.com>
2014-10-26 22:43:07 -0400

    output parsed csv in texts/create

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,11 +1,13 @@
 class TextsController < ApplicationController
   before_filter :authorize
+  require 'csv'
 
   def new
     @texts = Text.new
   end
 
   def create
-
+    file = params['text']['texts_csv']
+    @csv = CSV.parse(file.read)
   end
 end
