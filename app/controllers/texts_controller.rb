d7dd1fed3bdffb44c4a741f805c65603ddc885d9
Edwin <edwin.h.morris@gmail.com>
2014-10-30 22:19:41 -0400

    create field confirmation interface for text uploading
    throw in a quick validation, use translations for error

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -8,6 +8,16 @@ class TextsController < ApplicationController
 
   def create
     file = params['text']['texts_csv']
+    @field_names = ['sent_received',
+                    'timestamp',
+                    'contacts',
+                    'numbers',
+                    'message']
     @csv = CSV.parse(file.read)
+
+    if @csv.first.count < @field_names.count
+      flash[:error] = t('texts.invalid_file')
+      redirect_to new_text_path
+    end
   end
 end
