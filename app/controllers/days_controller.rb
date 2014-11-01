e5c6f5df4cda85d52deef9e042af56cfda3a9f40
Edwin <edwin.h.morris@gmail.com>
2014-10-31 22:33:42 -0400

    move texts into partial, render in meta pane

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -13,8 +13,9 @@ class DaysController < ApplicationController
     @today = Date.parse(storyline_day)
     @tomorrow = Date.parse(storyline_day) + 1
 
-    @texts = current_user.texts.where(
-      timestamp: @today.beginning_of_day..@today.end_of_day)
+    @texts = current_user.texts.
+      where(timestamp: @today.beginning_of_day..@today.end_of_day).
+      order(:timestamp)
 
     @instagram_photos = get_day_photos(storyline_day)
     @geodata_json, @places, @weather = nil
